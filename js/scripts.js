3805f9589d3ca0ef226cc8a8a599b6de7ed0536d
Ben Pawlik <james.benjamin.pawlik@gmail.com>
2021-06-10 15:34:45 -0700

    add function to convert index to numeral

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,31 @@
 
-let I = 1
-let V = 5
-let X = 10
-let L = 50
-Let C = 100
-Let D = 500
-Let M = 1000
+// let I = 1
+// let V = 5
+// let X = 10
+// let L = 50
+// Let C = 100
+// Let D = 500
+// Let M = 1000
 
 
-function numberToRomanNumeral(number) {
-  let romanNumerals = ['I','II','III','IV','V','VI','VII', 'VIII'
-, 'IX', 'X'] 
-  const numeralOutput = romanNumeral[i]
-  
-}
+function romanNumeral(number) {
+  const romanNumerals = ['I','II','III','IV','V','VI','VII', 'VIII'
+, 'IX', 'X']
+  const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  let a = numberArray.indexOf(number)
+  return romanNumerals[a]
+}  romanNumeral(5)
+
+// function romanNumeral(number) {
+//   const romanNumerals = ['I','II','III','IV','V','VI','VII', 'VIII'
+// , 'IX', 'X']
+//   const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//   let a = numberArray.indexOf(number)
+//   return romanNumerals[a]
+// }  romanNumeral(4)
+
+
+
+//   const romanNumerals = ['I','II','III','IV','V','VI','VII', 'VIII'
+// , 'IX', 'X']
+//   const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
